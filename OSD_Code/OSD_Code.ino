5941f7b7a71530f83661bf4256f889233edd158d
skaman82 <albert@wd-design.de>
2018-06-26 23:06:46 +0200

    osd warnings

--- OSD_Code/OSD_Code.ino
+++ OSD_Code/OSD_Code.ino
@@ -22,6 +22,9 @@ float Voltage;
 int32_t battery_health = 0;
 unsigned long Protocoltime = 0;
 unsigned long Layouttime = 0;
+unsigned long BatTime = 0;
+int BatWarningstate = 0;
+
 
 Max7456 osd;
 byte logo1[]={0xC0,0xC1,0xC2,0xC3,0xC4,0xC5,0xC6,0xC7,0xC8,0xC9,0xCA,0xCB,0xCC };
@@ -133,6 +136,10 @@ void checkChanges()
      }
   }
 
+void baticon()
+{
+ 
+ }
   
 void loop()
 {
@@ -178,30 +185,60 @@ void loop()
 
     
     // voltage printout
+    osd.printMax7456Char(0x7F,29,7); //black 
+    osd.printMax7456Char(0x7F,24,7); //black
+    
     if (battery_health == 4) 
     {
       osd.printMax7456Char(0x90,24,7);
-      osd.printMax7456Char(0x7F,29,7); //black 
     }
     else if (battery_health == 3) 
     {
       osd.printMax7456Char(0x91,24,7);
-      osd.printMax7456Char(0x7F,29,7); //black 
     }
      else if (battery_health == 2) 
     {
       osd.printMax7456Char(0x92,24,7);
-      osd.printMax7456Char(0x7F,29,7); //black 
     }
      else if (battery_health == 1) 
     {
       osd.printMax7456Char(0x93,24,7);
-      osd.printMax7456Char(0x7F,29,7); //black 
     }
-    else
+    
+     else if (battery_health == 0) 
     {
-      osd.printMax7456Char(0x93,24,7);
-      osd.printMax7456Char(0x7F,29,7); //black 
+    
+          unsigned long CurrentBat = millis();
+      if ((CurrentBat - BatTime >= 300))
+      {
+        BatTime = CurrentBat;
+        if (BatWarningstate == 0)  
+        {
+        BatWarningstate = 1;
+        }
+        else 
+        {
+        BatWarningstate = 0;
+        }
+      
+
+        if (BatWarningstate == 1) 
+        {
+        osd.printMax7456Char(0x93,24,7);
+        }
+        else 
+        {
+        
+        }
+           
+      }
+      
+     } 
+      
+      
+      else
+    {
+      
     }    
 
       
@@ -229,35 +266,32 @@ void loop()
   
   
 
-  osd.print("VOLTAGE",1,3);
-  osd.print(Osvoltage, 9, 3, 2, 1); // test for voltage
+ // osd.print("VOLTAGE",1,3);
+ // osd.print(Osvoltage, 9, 3, 2, 1); // test for voltage
   
-  osd.print("OSD LAY",1,4);
-  osd.print(OslayoutEEP, 9, 4, 1, 0); // test for OSD layout
+ // osd.print("OSD LAY",1,4);
+ // osd.print(OslayoutEEP, 9, 4, 1, 0); // test for OSD layout
 
-  osd.print("RSSI",1,5);
-  osd.print(OsRSSI, 9, 5, 2, 0); // test for RSSI strenght
+ // osd.print("RSSI",1,5);
+ // osd.print(OsRSSI, 9, 5, 2, 0); // test for RSSI strenght
 
-  osd.print("DVR",1,6);
-  osd.print(OsDVRstatus, 9, 6, 1, 0); // test for DVR status
+ // osd.print("DVR",1,6);
+ //  osd.print(OsDVRstatus, 9, 6, 1, 0); // test for DVR status
 
-  osd.print("BLINK",1,7);
-  osd.print(Osblinkosd, 9, 7, 1, 0); // test for alarm status
+ //  osd.print("BLINK",1,7);
+ // osd.print(Osblinkosd, 9, 7, 1, 0); // test for alarm status
 
-  osd.print("RS-A",1,8);
-  osd.print(OsRSSIavail, 9, 8, 1, 0); // test if RSSI is available
+ // osd.print("RS-A",1,8);
+ // osd.print(OsRSSIavail, 9, 8, 1, 0); // test if RSSI is available
 
-  osd.print("HEALTH",1,9);
-  osd.print(OsHealth, 9, 9, 1, 0); // test if battery-health is available
+ // osd.print("HEALTH",1,9);
+ // osd.print(OsHealth, 9, 9, 1, 0); // test if battery-health is available
   
   
   }
 
   
    if (newlayout == 2) {
-  //clearscreen();
-  //osd.print("POCKET-GROUNDSTATION",5,9);
-  //osd.print("WAITING...",10,11, true);
     
      oldlayout = 2;
 
@@ -292,19 +326,48 @@ void loop()
       osd.printMax7456Char(0x93,28,7);
       osd.printMax7456Char(0x7F,29,7); //black 
     }
-    else
+      else if (battery_health == 0) 
     {
-      osd.printMax7456Char(0x93,28,7, true);
-      osd.printMax7456Char(0x7F,29,7, true); //black 
-    }    
+      osd.printMax7456Char(0x7F,29,7); //black 
+      
+      unsigned long CurrentBat2 = millis();
+      if ((CurrentBat2 - BatTime >= 300))
+      {
+        BatTime = CurrentBat2;
+        if (BatWarningstate == 0)  
+        {
+        BatWarningstate = 1;
+        }
+        else 
+        {
+        BatWarningstate = 0;
+        }
+      
+
+        if (BatWarningstate == 1) 
+        {
+        osd.printMax7456Char(0x93,28,7);
+        }
+        else 
+        {
+        osd.printMax7456Char(0x7F,28,7); //black
+        }
+           
+      }
+
+    } 
+    else { }   
     
     
   // DVR printout
   if (DVRstatus == 1) 
   {
-    osd.printMax7456Char(0x99,28,8);
     osd.printMax7456Char(0x7F,29,8); //black 
-  }
+    osd.printMax7456Char(0x99,28,8);       
+   }
+
+      
+  
   else {}
 
  
